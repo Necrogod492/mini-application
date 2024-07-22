72ac942fe83a9bfb8b36edb71c2097d040c8c9cc
KRAUTH Mickael <mickaell.kr@gmail.com>
2024-07-22 15:04:57 +0200

    Fix server start/stop for tests

--- app.test.js
+++ app.test.js
@@ -1,8 +1,21 @@
 const request = require("supertest");
 const app = require("./app");
 
+let server;
+
+beforeAll((done) => {
+  server = app.listen(3000, () => {
+    console.log("Server running for tests");
+    done();
+  });
+});
+
+afterAll((done) => {
+  server.close(done);
+});
+
 test("GET /", async () => {
-  const response = await request(app).get("/");
+  const response = await request(server).get("/");
   expect(response.statusCode).toBe(200);
   expect(response.text).toBe("Hello World!");
 });
