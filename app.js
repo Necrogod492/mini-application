50eb8dcf069ff488246efd85ac0f4371916c288f
KRAUTH Mickael <mickaell.kr@gmail.com>
2024-07-24 12:28:40 +0200

    FEAT made logs work with Kibana

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
-// app.js
 const express = require("express");
 const app = express();
 const port = 3000;
+const winston = require("winston");
+const path = require("path");
+
+// Configuration de winston
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: path.join(__dirname, "app.log") }),
+  ],
+});
+
+app.use((req, res, next) => {
+  // Journaliser chaque requête
+  logger.info({
+    message: "HTTP Request",
+    method: req.method,
+    url: req.url,
+    timestamp: new Date().toISOString(),
+  });
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+  // Journaliser une réponse réussie
+  logger.info({
+    message: "Response sent",
+    method: req.method,
+    url: req.url,
+    statusCode: res.statusCode,
+    timestamp: new Date().toISOString(),
+  });
 });
 
 module.exports = app;
 
 if (require.main === module) {
   app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    // Journaliser le démarrage du serveur
+    logger.info(`Server running at http://localhost:${port}`);
   });
 }
