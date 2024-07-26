4aa20e32d1c6476573e4218b78c96ad4221d8f92
Noah.AY <75579315+Necrogod492@users.noreply.github.com>
2024-07-26 20:33:03 +0200

    first commit

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const logger = winston.createLogger({
   ],
 });
 
+app.use(express.json()); // To handle JSON payloads
+
 app.use((req, res, next) => {
   // Journaliser chaque requête
   logger.info({
@@ -22,6 +24,58 @@ app.use((req, res, next) => {
     timestamp: new Date().toISOString(),
   });
   next();
+});
+
+app.post("/add", (req, res) => {
+  const { a, b } = req.body;
+
+  if (typeof a === 'number' && typeof b === 'number') {
+    const sum = a + b;
+    res.status(200).json({ sum });
+    logger.info({
+      message: "success method add",
+      method: req.method,
+      url: req.url,
+      statusCode: res.statusCode,
+      timestamp: new Date().toISOString(),
+    });
+  } else { 
+    res.status(500).json({ error: "Invalid input, it seems that one of the 2 input fields is not a number" });
+    // Journaliser une erreur de validation
+    logger.error({
+      message: "Invalid input, it seems that one of the 2 input fields is not a number",
+      method: req.method,
+      url: req.url,
+      statusCode: res.statusCode,
+      timestamp: new Date().toISOString(),
+    });
+  }
+});
+
+app.post("/multiply", (req, res) => {
+  const { a, b } = req.body;
+
+  if (typeof a === 'number' && typeof b === 'number') {
+    const total = a * b;
+    res.status(200).json({ total });
+    logger.info({
+      message: "success method mutiply",
+      method: req.method,
+      url: req.url,
+      statusCode: res.statusCode,
+      timestamp: new Date().toISOString(),
+    });
+  } else { 
+    res.status(500).json({ error: "Invalid input, it seems that one of the 2 input fields is not a number" });
+    // Journaliser une erreur de validation
+    logger.error({
+      message: "Invalid input, it seems that one of the 2 input fields is not a number",
+      method: req.method,
+      url: req.url,
+      statusCode: res.statusCode,
+      timestamp: new Date().toISOString(),
+    });
+  }
 });
 
 app.get("/", (req, res) => {
--- app.test.js
+++ app.test.js
@@ -19,3 +19,41 @@ test("GET /", async () => {
   expect(response.statusCode).toBe(200);
   expect(response.text).toBe("Hello World!");
 });
+
+test("POST /add", async () => {
+  const response = await request(server)
+    .post("/add")
+    .send({ a: 5, b: 50 });
+
+  expect(response.statusCode).toBe(200);
+});
+
+test("POST /add wrong input", async () => {
+  const response = await request(server)
+    .post("/add")
+    .send({ a: "pikapika", b: 263 });
+
+  expect(response.statusCode).toBe(500);
+  expect(response.body.error).toBe(
+    "Invalid input, it seems that one of the 2 input fields is not a number"
+  );
+});
+
+test("POST /multiply", async () => {
+  const response = await request(server)
+    .post("/multiply")
+    .send({ a: 5, b: 50 });
+
+  expect(response.statusCode).toBe(200);
+});
+
+test("POST /multiply wrong input", async () => {
+  const response = await request(server)
+    .post("/multiply")
+    .send({ a: "test", b: 75 });
+
+  expect(response.statusCode).toBe(500);
+  expect(response.body.error).toBe(
+    "Invalid input, it seems that one of the 2 input fields is not a number"
+  );
+});
